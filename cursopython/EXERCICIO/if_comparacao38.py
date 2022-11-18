116a5fb36bf9154a877e8feba47f0e5752e76406
Marco Antonio de Sena Sena <msena2210@gmail.com>
2022-11-18 19:09:01 -0300

    Exercicio

---
+++ cursopython/EXERCICIO/if_comparacao38.py
@@ -0,0 +1,19 @@
+"""
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(
+        f'{primeiro_valor=} é maior ou igual '
+        f'ao que {segundo_valor=}'
+    )
+else:
+    print(
+        f'{segundo_valor=} é maior '
+        f'do que {primeiro_valor=}'
+    )
+"""
+print(True and 1)
+print(1 and False)
+if 1 and 1:
+    print(True and 1 and False)
